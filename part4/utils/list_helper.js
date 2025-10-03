7bb7f083257f8eec0300ea98228d7ef83c3fb6e0
Egoitz Abilleira <egoitzabilleira@protonmail.com>
2025-10-03 18:05:23 +0100

    part4.5

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -6,7 +6,20 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0);
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return null;
+  }
+  const favorite = blogs.reduce(
+    (fav, blog) => (blog.likes > fav.likes ? blog : fav), blogs[0]
+  );
+  return {
+    title: favorite.title, author: favorite.author, likes: favorite.likes
+  };
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
