f9005db4f1ebec92867c2bd559f03a8fe473efd4
Egoitz Abilleira <egoitzabilleira@protonmail.com>
2025-10-09 18:57:27 +0100

    part4.17

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,13 +1,15 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
+
 
 blogsRouter.get('/', async (_request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
 blogsRouter.get('/:id', async (_request, response) => {
-  const blog = await Blog.findById(_request.params.id)
+  const blog = await Blog.findById(_request.params.id).populate('user', { username: 1, name: 1 }) 
   if (blog) {
     response.json(blog)
   } else {
@@ -16,9 +18,25 @@ blogsRouter.get('/:id', async (_request, response) => {
 })
 
 blogsRouter.post('/', async (request, response) => {
-  const blog = new Blog(request.body)
+  const user = await User.findOne({}) // Temporary: associate all blogs to the first user
+  if (!user) {
+    return response.status(400).json({ error: 'no users found to associate blog with' })
+  }
+  const blog = new Blog({
+    title: request.body.title,
+    author: request.body.author,
+    url: request.body.url,
+    likes: request.body.likes || 0,
+    user: user._id
+  })
+
   const savedBlog = await blog.save()
-  response.status(201).json(savedBlog)
+
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
+  const populated = await savedBlog.populate('user', { username: 1, name: 1 })
+  response.status(201).json(populated)
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
