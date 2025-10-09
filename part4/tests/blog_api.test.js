09a36c25fc0284f0809283af34af5bb98bdfe147
Egoitz Abilleira <egoitzabilleira@protonmail.com>
2025-10-09 13:29:21 +0100

    part4.12 done

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -94,3 +94,50 @@ test('if likes property is missing, it will default to 0', async () => {
   assert.ok(addedBlog)
   assert.strictEqual(addedBlog.likes, 0)
 })
+
+test('blog without title and url is not added', async () => {
+  const newBlog = {
+    author: 'No Title and URL',
+    likes: 3,
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length)
+})
+
+test ('a blog without url is not added', async () => {
+  const newBlog = {
+    title: 'No URL Blog',
+    author: 'Author',
+    likes: 2,
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length)
+})
+
+test ('a blog without title is not added', async () => {
+  const newBlog = {
+    author: 'Author',
+    url: 'http://example.com/no-title-blog',
+    likes: 2,
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length)
+})
